00af81f557a9c7492819b9f7c6d2e41201f7acd5
Albin-or <albinoropezadev@gmail.com>
2026-04-24 22:43:10 -0400

    Added JavaScript and fixed indentations

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-fetch('https://partsouq.com/en/catalog/genuine/vehicle?c=Toyota&ssd=%24%2AKwG9iZj81c3qpeTWx_K8u-Xx0dbIuba7uqiHtPz6yd3BxcfEgZa60P76_s_ZwNGXioC9tMrH3cHFx5f4l8rKpsvfod7MqK6t687yquztq7H2-vW6v7q7v6ylq_SxsK20qarfz9vKm_ror6zk6620qamTnfW_vsWlzsvBz4X66K-s_-uttKnlo7Soo66r7ubqq7H27PbyAAAAAAb-9ow%3D%24&vid=0&q=').then((response) => {
-  console.log(response.ok) // true si la petición salió bien
-  console.log(response.status) // código HTTP, por ejemplo 200
+fetch('navigation.html')
+.then((response) => {
+    return response.text();
+})
+.then((data) => {
+    document.querySelector('#navigation').innerHTML = data;
 })
